6c66ce1891facb8403035384cbb48c52745d13f3
set404 <89992008819@mail.ru>
2023-06-01 15:39:01 +0300

    Initial commit

---
+++ src/main/java/ru/kotomore/springbootmarket/SpringBootMarketApplication.java
@@ -0,0 +1,13 @@
+package ru.kotomore.springbootmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kotomore/springbootmarket/SpringBootMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kotomore.springbootmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
